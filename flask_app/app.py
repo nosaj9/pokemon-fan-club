2d7fda99ae577ab9f103ee58955214b5e7101778
BuildTools <jdcomman27@gmail.com>
2022-12-11 17:52:55 -0500

    added basic pokemon functionality

---
+++ flask_app/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+from model import PokeClient
+app = Flask(__name__)
+
+poke_client = PokeClient()
+
+@app.route('/')
+def index():
+    """
+    Must show all of the pokemon names as clickable links
+
+    Check the README for more detail.
+    """
+    
+    return render_template('index.html', pokemon_list=poke_client.get_pokemon_list())
+
+@app.route('/pokemon/<pokemon_name>')
+def pokemon_info(pokemon_name):
+    """
+    Must show all the info for a pokemon identified by name
+
+    Check the README for more detail
+    """
+
+    return render_template('pokemon.html', pokemon=poke_client.get_pokemon_info(pokemon_name))
+
+@app.route('/ability/<ability_name>')
+def pokemon_with_ability(ability_name):
+    """
+    Must show a list of pokemon 
+
+    Check the README for more detail
+    """
+    
+    return render_template('ability.html', ability=ability_name, ability_pokemon=poke_client.get_pokemon_with_ability(ability_name))
