99a71ad6dc1f722ebfd9e19f79b8a5ebcfb9bf82
BuildTools <jdcomman27@gmail.com>
2022-12-13 14:59:30 -0500

    moved everything to routes

--- flask_app/app.py
+++ flask_app/app.py
@@ -42,7 +42,9 @@ app.config["MONGODB_HOST"] = "mongodb://localhost:27017/pokemon_rater"
 # main = Blueprint("main", __name__)
 # app.register_blueprint(main)
 from users.routes import users
+from pokemon.routes import pokemon
 app.register_blueprint(users)
+app.register_blueprint(pokemon)
 
 db.init_app(app)
 login_manager.init_app(app)
@@ -53,42 +55,42 @@ from model import PokeClient
 poke_client = PokeClient()
 
 
-from models import User, load_user
-from forms import (
-    PokemonCommentForm,
-    RegistrationForm,
-    LoginForm,
-    UpdateUsernameForm,
-)
+# from models import User, load_user
+# from forms import (
+#     PokemonCommentForm,
+#     RegistrationForm,
+#     LoginForm,
+#     UpdateUsernameForm,
+# )
 
-from model import PokeClient
+# from model import PokeClient
 
-@app.route('/')
-def index():
-    """
-    Must show all of the pokemon names as clickable links
+# @app.route('/')
+# def index():
+#     """
+#     Must show all of the pokemon names as clickable links
 
-    Check the README for more detail.
-    """
+#     Check the README for more detail.
+#     """
     
-    return render_template('index.html', pokemon_list=poke_client.get_pokemon_list())
+#     return render_template('index.html', pokemon_list=poke_client.get_pokemon_list())
 
-@app.route('/pokemon/<pokemon_name>')
-def pokemon_info(pokemon_name):
-    """
-    Must show all the info for a pokemon identified by name
+# @app.route('/pokemon/<pokemon_name>')
+# def pokemon_info(pokemon_name):
+#     """
+#     Must show all the info for a pokemon identified by name
 
-    Check the README for more detail
-    """
+#     Check the README for more detail
+#     """
 
-    return render_template('pokemon.html', pokemon=poke_client.get_pokemon_info(pokemon_name))
+#     return render_template('pokemon.html', pokemon=poke_client.get_pokemon_info(pokemon_name))
 
-@app.route('/ability/<ability_name>')
-def pokemon_with_ability(ability_name):
-    """
-    Must show a list of pokemon 
+# @app.route('/ability/<ability_name>')
+# def pokemon_with_ability(ability_name):
+#     """
+#     Must show a list of pokemon 
 
-    Check the README for more detail
-    """
+#     Check the README for more detail
+#     """
     
-    return render_template('ability.html', ability=ability_name, ability_pokemon=poke_client.get_pokemon_with_ability(ability_name))
+#     return render_template('ability.html', ability=ability_name, ability_pokemon=poke_client.get_pokemon_with_ability(ability_name))
